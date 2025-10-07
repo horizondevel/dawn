9e53dd066c5cf1de37808e405ed847a65cc1e3cf
sammehdevel <sharrison.devel@gmail.com>
2025-10-07 11:57:06 -0600

    Static file serving

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
-use axum::routing::get;
 use axum::Router;
+use tower_http::services::{ServeDir, ServeFile};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let app = Router::new().route("/", get(hello));
+    let static_serve = ServeDir::new("static")
+        .not_found_service(ServeFile::new("static/index.html"));
+    let app = Router::new()
+        .route_service("/", ServeFile::new("static/index.html"))
+        .nest_service("/static", static_serve)
+    ;
     let listener = tokio::net::TcpListener::bind("0.0.0.0:9999").await?;
     axum::serve(listener, app).await?;
     Ok(())
-}
-
-async fn hello() -> String {
-    "Hello, world!".to_string()
 }
